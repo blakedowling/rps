14aed505cfb57cfd4aa7f55a7869856210661025
Blake Dowling <blakeddowling@gmail.com>
2016-11-01 20:01:03 +0000

    pictures modified

--- rps.js
+++ rps.js
@@ -1,24 +1,30 @@
+//what to improve
+//1. get a new header image so the text can be read more easily
+//2. make a scoreboard
+//3. indicate with "win" or "tie" who won or lost
+//4. have the "you're a phony" text cycle through different funny phrases
+
 $(document).ready(function() {
 
     var donald = false;
     var hillary = false;
     var date = new Date();
-    var hours = date.getHours();
+    //var hours = date.getHours();
     var you = 0;
     var enemy = 0;
     var donaldCount = 0;
     var hillaryCount = 0;
     var computerChoice;
-    console.log("Hours: " + hours);
-    if (hours < 12) {
-        $("#visible").html("Good morning! Choose your player.");
-    } else if (hours < 17) {
-        $('#visible').html("Good afternoon! Choose your player.");
-    } else {
-        $("#visible").html("Good evening! Choose your player.");
-    }
+    //console.log("Hours: " + hours);
+    // if (hours < 12) {
+    //     $("#visible").html("Good morning! Choose your player.");
+    // } else if (hours < 17) {
+    //     $('#visible').html("Good afternoon! Choose your player.");
+    // } else {
+    //     $("#visible").html("Good evening! Choose your player.");
+    // }
     
-    var roc = function() { $('#rps').html("Rock..."); }
+    var roc = function() { console.log("it should say 'rock..."); $('#rps').html("Rock..."); }
     var pap = function() { $('#rps').html("Paper..."); }
     var sci = function() { $('#rps').html("Scissors..."); }
     var shoo = function() { $('#rps').html("Shoot!"); }
@@ -26,11 +32,16 @@ $(document).ready(function() {
     //this function reenables all buttons and returns the pictures to their original
     var enableRPS = function() {
         $('body').css("pointer-events", "auto");
-        $('#rps').html("Choose again!");
-        $('.donald').attr("src", "donald-trump.jpg");
-        $('.hillary').attr("src", "hillary-clinton-thumbs-up.jpg")
+        $('.donald').css("pointer-events", "auto");
+        $('.hillary').css("pointer-events", "auto");
+        $("#donaldChoice").html("");
+        $("#hillaryChoice").html("");
+        $('#rps').html("");
+        donaldPic();
+        hillaryPic();
     }
     
+    //testing this
     var computer = function() {
         computerChoice = Math.random();
         if (computerChoice < 0.34) {
@@ -44,70 +55,109 @@ $(document).ready(function() {
     }
     
     var shoot = function() {
-        $('#visible').html("...")
         console.log("shoot function called");
         $('body').css("pointer-events", "none");
-        roc();
-        setTimeout(pap, 750);
-        setTimeout(sci, 1500);
-        setTimeout(shoo, 2250);
+        setTimeout(roc, 400);
+        setTimeout(pap, 1000);
+        setTimeout(sci, 1600);
+        setTimeout(shoo, 2200);
     };
     
-    var hideRockAndPaper = function() {
-        $('.rock').css("opacity", 0);
-        $('.paper').css("opacity", 0);
-    };
+    var RPSfadeOut = function() {
+        $('.rock').fadeOut();
+        $('.paper').fadeOut();
+        $('.scissors').fadeOut();
+        $('#2of3').fadeOut();
+    }
     
-    var showRockAndPaper = function() {
-        $('.rock').css("opacity", 1);
-        $('.paper').css("opacity", 1);
-    };
+    var RPSfadeIn = function() {
+        $('.rock').fadeIn();
+        $('.paper').fadeIn();
+        $('.scissors').fadeIn();
+        $('#2of3').fadeIn();
+        
+    }
     
-    var hideRockAndScissors = function() {
-        $('.rock').css("opacity", 0);
-        $('.scissors').css("opacity", 0);
-    };
+    var donaldPic = function() {
+        $('.donald').show();
+        $('.donaldPaper').hide();
+        $('.donaldScissors').hide();
+        $('.donaldRock').hide();
+    }
     
-    var showRockAndScissors = function() {
-        $('.rock').css("opacity", 1);
-        $('.scissors').css("opacity", 1);
-    };
+    var donaldRock = function() {
+        $('.donald').hide();
+        $('.donaldPaper').hide();
+        $('.donaldScissors').hide();
+        $('.donaldRock').show();
+        $('#donaldChoice').html("Rock");
+    }
+    var donaldPaper = function() {
+        $('.donald').hide();
+        $('.donaldPaper').show();
+        $('.donaldScissors').hide();
+        $('.donaldRock').hide();
+        $('#donaldChoice').html("Paper");
+    }
+    var donaldScissors = function() {
+        $('.donald').hide();
+        $('.donaldPaper').hide();
+        $('.donaldScissors').show();
+        $('.donaldRock').hide();
+        $('#donaldChoice').html("Scissors");
+    }
     
-    var hidePaperAndScissors = function() {
-        $('.paper').css("opacity", 0);
-        $('.scissors').css("opacity", 0);
-    };
-    
-    var showPaperAndScissors = function() {
-        $('.paper').css("opacity", 1);
-        $('.scissors').css("opacity", 1);
-    };
-    
-    var donaldRock = function() { $('.donald').attr('src', 'donald-rock.jpeg'); $('#visible').html("Donald chose rock!"); }
-    var donaldPaper = function() { $('.donald').attr('src', 'donald-paper.png'); $('#visible').html("Donald chose paper!"); }
-    var donaldScissors = function() { $('.donald').attr('src', 'donald-scissors.jpg'); $('#visible').html("Donald chose scissors!"); }
-    var hillaryRock = function() { $('.hillary').attr('src', 'hillary-rock.jpg'); $('#visible').html("Hillary chose rock!"); }
-    var hillaryPaper = function() { $('.hillary').attr('src', 'hillary-paper.jpg'); $('#visible').html("Hillary chose paper!"); }
-    var hillaryScissors = function() { $('.hillary').attr('src', 'hillary-scissors.jpg'); $('#visible').html("Hillary chose scissors!"); }
+    var hillaryPic = function() {
+        $('.hillary').show();
+        $('.hillaryRock').hide();
+        $('.hillaryPaper').hide();
+        $('.hillaryScissors').hide();
+        
+    }
+    var hillaryRock = function() {
+        $('.hillary').hide();
+        $('.hillaryRock').show();
+        $('.hillaryPaper').hide();
+        $('.hillaryScissors').hide();
+        $('#hillaryChoice').html("Rock");
+    }
+    var hillaryPaper = function() {
+        $('.hillary').hide();
+        $('.hillaryRock').hide();
+        $('.hillaryPaper').show();
+        $('.hillaryScissors').hide();
+        $('#hillaryChoice').html("Paper");
+    }
+    var hillaryScissors = function() {
+        $('.hillary').hide();
+        $('.hillaryRock').hide();
+        $('.hillaryPaper').hide();
+        $('.hillaryScissors').show();
+        $('#hillaryChoice').html("Scissors");
+    }
     
     var disable = function() {
         donaldCount = 0;
         hillaryCount = 0;
-        $('.donald').attr('src', 'donald-trump.jpg');
-        $('.hillary').attr('src', 'hillary-clinton-thumbs-up.jpg');
+        donaldPic();
+        hillaryPic();
         $("#donaldCount").html(donaldCount);
         $("#hillaryCount").html(hillaryCount);
+        $("#donaldChoice").html("");
+        $("#hillaryChoice").html("");
         $('#hidden').hide();
         $('.hillary').css("box-shadow", "0 0 0 0 black");
         $('.donald').css("box-shadow", "0 0 0 0 black");
-        $("#visible").html("Choose your player.");
+        $("#visible").html("Choose your player");
         $('#rps').hide();
+        $('#2of3').html("");
         console.log("disable function called");
     };
     
     var showRPS = function() {
         console.log('showRPS function called');
-        $('#rps').show().html("Let the games begin!");
+        $('#rps').show();
+        $('#2of3').html('Best 2 out of 3');
         $('#hidden').show();
     };
     
@@ -157,10 +207,15 @@ $(document).ready(function() {
     
     $('.donald').click(function() {
         presidentClick();
-        $("#visible").html("You're a phony and a fraud! Good luck.");
-        $("#enemyName").html("Hillary");
-        $('.donald').css("box-shadow", "0px 0px 10px 5px #003777");
+        $("#visible").html("You're a phony and a fraud!");
+        $('.donald').css("box-shadow", "0px 0px 10px 6px #003777");
+        $('.donaldRock').css("box-shadow", "0px 0px 10px 6px #003777");
+        $('.donaldPaper').css("box-shadow", "0px 0px 10px 6px #003777");
+        $('.donaldScissors').css("box-shadow", "0px 0px 10px 6px #003777");
         $('.hillary').css("box-shadow", "0 0 0 0 black");
+        $('.hillaryRock').css("box-shadow", "0 0 0 0 black");
+        $('.hillaryPaper').css("box-shadow", "0 0 0 0 black");
+        $('.hillaryScissors').css("box-shadow", "0 0 0 0 black");
         donald = true;
         hillary = false;
         
@@ -168,10 +223,15 @@ $(document).ready(function() {
     
     $('.hillary').click(function() {
         presidentClick();
-        $("#visible").html("Liars never win! You can still try though.");
-        $("#enemyName").html("Donald");
-        $('.hillary').css("box-shadow", "0px 0px 10px 5px #003777");
+        $("#visible").html("Liars never win!");
+        $('.hillary').css("box-shadow", "0px 0px 10px 6px #003777");
+        $('.hillaryRock').css("box-shadow", "0px 0px 10px 6px #003777");
+        $('.hillaryPaper').css("box-shadow", "0px 0px 10px 6px #003777");
+        $('.hillaryScissors').css("box-shadow", "0px 0px 10px 6px #003777");
         $('.donald').css("box-shadow", "0 0 0 0 black");
+        $('.donaldRock').css("box-shadow", "0 0 0 0 black");
+        $('.donaldPaper').css("box-shadow", "0 0 0 0 black");
+        $('.donaldScissors').css("box-shadow", "0 0 0 0 black");
         hillary = true;
         donald = false;
     });
@@ -180,25 +240,25 @@ $(document).ready(function() {
     
     $('.rock').click(function() {
         shoot();
-        hidePaperAndScissors();
-        setTimeout(showPaperAndScissors, 6000);
-        setTimeout(enableRPS, 6000);
+        RPSfadeOut();
+        setTimeout(RPSfadeIn, 5000);
+        setTimeout(enableRPS, 5000);
         var userChoice = "rock";
         computer();
         
         if(donald) {
-            setTimeout(donaldRock, 2250);
+            setTimeout(donaldRock, 2200);
         } else {
-            setTimeout(hillaryRock, 2250);
+            setTimeout(hillaryRock, 2200);
         }
     
         if (userChoice === computerChoice)
         {
             if (donald){
-                setTimeout(hillaryRock, 2250);
+                setTimeout(hillaryRock, 2200);
             }
             else {
-                setTimeout(donaldRock, 2250);
+                setTimeout(donaldRock, 2200);
             }
             
         }
@@ -206,11 +266,11 @@ $(document).ready(function() {
         {
             you += 1;
             if (donald){
-                setTimeout(hillaryScissors, 2250);
+                setTimeout(hillaryScissors, 2200);
                 donaldCount +=1;
             }
             else {
-                setTimeout(donaldScissors, 2250);
+                setTimeout(donaldScissors, 2200);
                 hillaryCount +=1;
             }
         }
@@ -219,50 +279,50 @@ $(document).ready(function() {
             enemy += 1;
             console.log(enemy);
             if (donald){
-                setTimeout(hillaryPaper, 2250);
+                setTimeout(hillaryPaper, 2200);
                 hillaryCount +=1;
             }
             else {
-                setTimeout(donaldPaper, 2250);
+                setTimeout(donaldPaper, 2200);
                 donaldCount +=1;
             }
         }
-        setTimeout(score, 2250);
+        setTimeout(score, 2200);
         setTimeout(gameOver, 4500);
     });
     
     $('.paper').click(function() {
         shoot();
-        hideRockAndScissors();
-        setTimeout(showRockAndScissors, 6000);
-        setTimeout(enableRPS, 6000);
+        RPSfadeOut();
+        setTimeout(RPSfadeIn, 5000);
+        setTimeout(enableRPS, 5000);
         computer();
         var userChoice = "paper";
         
         if (donald) {
-            setTimeout(donaldPaper, 2250);
+            setTimeout(donaldPaper, 2200);
         } else {
-            setTimeout(hillaryPaper, 2250);
+            setTimeout(hillaryPaper, 2200);
         }
         
         if(computerChoice === "paper") 
         {
             if (donald){
-                setTimeout(hillaryPaper, 2250);
+                setTimeout(hillaryPaper, 2200);
             }
             else {
-                setTimeout(donaldPaper, 2250);
+                setTimeout(donaldPaper, 2200);
             }
         }
         else if(computerChoice === "rock")
         {
             you += 1;
             if (donald){
-                setTimeout(hillaryRock, 2250);
+                setTimeout(hillaryRock, 2200);
                 donaldCount += 1;
             }
             else {
-                setTimeout(donaldRock, 2250);
+                setTimeout(donaldRock, 2200);
                 hillaryCount += 1;
             }
         }
@@ -270,50 +330,50 @@ $(document).ready(function() {
         {
             enemy += 1;
             if (donald){
-                setTimeout(hillaryScissors, 2250);
+                setTimeout(hillaryScissors, 2200);
                 hillaryCount += 1;
             }
             else {
-                setTimeout(donaldScissors, 2250);
+                setTimeout(donaldScissors, 2200);
                 donaldCount += 1;
             }
         }
-        setTimeout(score, 2250)
+        setTimeout(score, 2200)
         setTimeout(gameOver, 4500);
     });
     
     $('.scissors').click(function() {
         shoot();
-        hideRockAndPaper();
-        setTimeout(showRockAndPaper, 6000);
-        setTimeout(enableRPS, 6000);
+        RPSfadeOut();
+        setTimeout(RPSfadeIn, 5000);
+        setTimeout(enableRPS, 5000);
         computer();
         var userChoice = "scissors";
         
         if (donald) {
-            setTimeout(donaldScissors, 2250);
+            setTimeout(donaldScissors, 2200);
         } else {
-            setTimeout(hillaryScissors, 2250);
+            setTimeout(hillaryScissors, 2200);
         }
         
         if(computerChoice === "scissors")
         {
             if (donald){
-                setTimeout(hillaryScissors, 2250);
+                setTimeout(hillaryScissors, 2200);
             }
             else {
-                setTimeout(donaldScissors, 2250);
+                setTimeout(donaldScissors, 2200);
             }
         }
         else if(computerChoice === "rock")
         {
             enemy += 1;
             if (donald){
-                setTimeout(hillaryRock, 2250);
+                setTimeout(hillaryRock, 2200);
                 hillaryCount += 1;
             }
             else {
-                setTimeout(donaldRock, 2250);
+                setTimeout(donaldRock, 2200);
                 donaldCount += 1;
             }
         }
@@ -321,18 +381,18 @@ $(document).ready(function() {
         {
             you += 1;
             if (donald){
-                setTimeout(hillaryPaper, 2250);
+                setTimeout(hillaryPaper, 2200);
                 donaldCount += 1;
             }
             else {
-                setTimeout(donaldPaper, 2250);
+                setTimeout(donaldPaper, 2200);
                 hillaryCount += 1;
             }
         }
-        $('#donaldCount').delay(6000).queue(function(n){
+        $('#donaldCount').delay(5000).queue(function(n){
             $(this).html(donaldCount);
         });
-        setTimeout(score, 2250);
+        setTimeout(score, 2200);
         setTimeout(gameOver, 4500);
     });
 });
